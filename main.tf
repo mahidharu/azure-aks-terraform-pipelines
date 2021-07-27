1e3dc6263f0f9d8694378ee76428e421aa0f1c7e
Mahidhar <43684464+mahidharu@users.noreply.github.com>
2021-07-27 01:21:46 -0500

    Adding Terraform in pipeline

--- main.tf
+++ main.tf
@@ -9,31 +9,10 @@ terraform {
 
 provider "azurerm" {
   # Configuration options
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
   features {}
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "${var.prefix}-k8s-resources"
+  name     = "${var.prefix}-example-resources"
   location = var.location
-}
-
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "${var.prefix}-k8s"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "${var.prefix}-k8s"
-
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_DS2_v2"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
 }
---
+++ storage.tf
@@ -0,0 +1,12 @@
+
+/*resource "azurerm_storage_account" "example" {
+  name                     = "${var.prefix}-sa"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "dev"
+  }
+}*/
