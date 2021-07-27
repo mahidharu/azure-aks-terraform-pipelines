cf1c970238d85fccca1d83b61e415b4396c5ec87
Mahidhar <43684464+mahidharu@users.noreply.github.com>
2021-07-27 02:05:28 -0500

    Update type format

--- variables.tf
+++ variables.tf
@@ -9,17 +9,17 @@ variable "location" {
 }
 
 variable "arm_subscription_id" {
-  type = "string"
+  type = string
 }
 
 variable "arm_client_id" {
-  type = "string"
+  type = string
 }
 
 variable "arm_client_secret" {
-  type = "string"
+  type = string
 }
 
 variable "arm_tenant_id" {
-  type = "string"
+  type = string
 }
